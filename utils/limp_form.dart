162f15c77ed0bab83da29b7cffa5f70ea2869450
paco <paco.33353@gmail.com>
2021-01-04 14:16:30 -0600

    ya detecta numeros de varios digitos

--- utils/limp_form.dart
+++ utils/limp_form.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import "package:test/test.dart";
 
 // TODO: debe detectar numerosa de mas de un digito
@@ -5,25 +7,33 @@ import "package:test/test.dart";
 List<dynamic> convertStL(String s) {
   List<dynamic> b = [];
 
+  var i1 = new List();
+  loop:
   for (var i = 0; i < s.length; i++) {
-    //Convierte los numeros del string a num si se puede
     if (int.tryParse(s[i]) is int) {
+      //Convierte los numeros del string a num si se puede
+      i1.add(i);
       String numero;
       num s1;
-      if (int.tryParse(s[i + 1]) is int) {
-        numero = s.substring(i, i + 2);
+      int i2 = i;
+      if (i == s.length - 1) {
+        numero = s.substring(i1.first, i2 + 1);
         s1 = int.parse(numero);
         b.add(s1);
-        continue;
+        i1.clear();
+        break loop;
       }
-      if (int.tryParse(s[i - 1]) is int) {
-        continue;
-      } else {
-        s1 = int.parse(s[i]);
-        b.add(s1);
-      }
-      // print(s1);
 
+      if (int.tryParse(s[i + 1]) is int) {
+        i2++;
+        continue loop;
+      } else {
+        // print(i1);
+        numero = s.substring(i1.first, i2 + 1);
+        s1 = int.parse(numero);
+        b.add(s1);
+        i1.clear();
+      }
     } else if (s[i] == '×') {
       b.add('*');
     } else if (s[i] == '÷') {
@@ -38,7 +48,7 @@ List<dynamic> convertStL(String s) {
 }
 
 main(List<String> args) {
-  var f = '411 + 13 - 5)';
+  var f = '4116445 + 134848 - 6545448';
   List l = convertStL(f);
   for (var i in l) {
     print('el valor es: $i y su tipo es: ${i.runtimeType}');
