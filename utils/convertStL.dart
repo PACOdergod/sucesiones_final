7660eea68fa4f7c3ccfda24cebc97f35bc9c16fc
paco <paco.33353@gmail.com>
2021-01-10 22:50:22 -0600

    Ya detecta perfectamente los puntos incluso en el primer numero :)

--- utils/convertStL.dart
+++ utils/convertStL.dart
@@ -6,35 +6,41 @@ List<dynamic> convertStL(String s) {
 
   for (var i = 0; i < s.length; i++) {
     //Convierte los numeros del string a num si se puede
-    if (int.tryParse(s[i]) is int) {
+    if (int.tryParse(s[i]) is int || s[i] == '.') {
       String numero;
       num s1;
 
-      if (i == 0 && int.tryParse(s[i + 1]) == null) {
-        s1 = int.parse(s[i]);
+      if (i == 0 && num.tryParse(s[i + 1]) == null) {
+        aux.add(i);
+        if (s[i + 1] == '.') {
+          continue;
+        }
+        s1 = num.parse(s[i]);
         b.add(s1);
         continue;
       }
-      if (int.tryParse(s[i + 1]) is int) {
-        aux.add(i);
-        print(aux);
 
+      if (num.tryParse(s[i + 1]) is int) {
+        aux.add(i);
         continue;
       }
-      if (int.tryParse(s[i + 1]) == null && aux.isNotEmpty) {
+
+      if (num.tryParse(s[i + 1]) == null && aux.isNotEmpty) {
+        if (s[i + 1] == '.') {
+          continue;
+        }
         numero = s.substring(aux.first, i + 1);
         print(numero);
-        s1 = int.parse(numero);
+        s1 = num.parse(numero);
         b.add(s1);
         aux.clear();
       }
-
-      if (int.tryParse(s[i - 1]) is int) {
-        continue;
-      } else {
-        s1 = int.parse(s[i]);
+      //
+      else {
+        s1 = num.parse(s[i]);
         b.add(s1);
       }
+      continue;
       // print(s1);
 
     } else if (s[i] == '×') {
@@ -51,10 +57,10 @@ List<dynamic> convertStL(String s) {
 }
 
 main(List<String> args) {
-  var f = '454 + 543(1×345(5× 2)(3 + 5(4 ÷ 2554.545) + 3 - 2(3 × 2) + 1) - 5)';
+  var f = '41.54 + 5(1×3(5454× 2)(3 + 5(4 ÷ 47.545) + 3 - 2(3 × 2) + 1) - 5)';
   List l = convertStL(f);
-  for (var i in l) {
-    print('el valor es: $i y su tipo es: ${i.runtimeType}');
-  }
+  // for (var i in l) {
+  //   print('el valor es: $i y su tipo es: ${i.runtimeType}');
+  // }
   print(l);
 }
